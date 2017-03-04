e4d3bd42893c6c934f5666a03b428e93db623100
kantorv <kantorvv@gmail.com>
2017-03-04 02:01:49 +0200

    unnessesary code removed

--- nodemcu_server.ino
+++ nodemcu_server.ino
@@ -41,35 +41,6 @@ int brightness = 100;
 String rgb_value;
 int redValue, greenValue, blueValue;
 
-void handleRoot() {
-  digitalWrite ( led, 1 );
-  char temp[400];
-  int sec = millis() / 1000;
-  int min = sec / 60;
-  int hr = min / 60;
-
-  snprintf ( temp, 400,
-
-"<html>\
-  <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>ESP8266 Demo</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>Hello from ESP8266!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <img src=\"/test.svg\" />\
-  </body>\
-</html>",
-
-    hr, min % 60, sec % 60
-  );
-  server.send ( 200, "text/html", temp );
-  digitalWrite ( led, 0 );
-}
 
 void handleNotFound() {
   digitalWrite ( led, 1 );
@@ -121,15 +92,13 @@ void setup ( void ) {
   digitalWrite ( led, 0 );
 
 
-  server.on ( "/", handleRoot );
-  server.on ( "/test.svg", drawGraph );
+  server.on ( "/", openPicker );
   server.on ( "/settings", retreiveCurrentSettings );
   server.on ( "/setcolor", handleColor );
   server.on ( "/brightness", handleBrightness );
   server.on ( "/inline", []() {
     server.send ( 200, "text/plain", "this works as well" );
   } );
-  server.on ( "/color", openPicker );
   server.onNotFound ( handleNotFound );
   server.begin();
   Serial.println ( "HTTP server started" );
@@ -161,7 +130,7 @@ String  hex2rgb(String str){
     blueValue = b;
     return out;
 
-  }
+}
 
 void  setValue(int r, int g, int b, int brightness){
   //   g = 255 - g;
@@ -174,6 +143,9 @@ void  setValue(int r, int g, int b, int brightness){
   analogWrite(bluePin, b*brightness/100);
 
 }
+
+
+
 
 
 String getValue(String data, char separator, int index)
@@ -192,13 +164,17 @@ String getValue(String data, char separator, int index)
     return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
+
+
+
 void retreiveCurrentSettings(){
   String message =  "{\"result\":\"OK\",\"brightness\":\""+String(brightness)+"\",\"rgb\":\""+rgb_value+"\"}";
    server.send ( 200, "application/json", message );
 }
 
-void handleBrightness(){
 
+
+void handleBrightness(){
   String message = "brightness not  found";
   for ( uint8_t i = 0; i < server.args(); i++ ) {
     if( server.argName ( i )  == "brightness"){
@@ -217,24 +193,11 @@ void handleBrightness(){
           setValue(red*4,green*4,blue*4, brightness);
       }
       else{
-
         message = "No prev RGB value stored";
-    }
-
-
-
-     //  setValue(red*4,green*4,blue*4);
-
-
-
-
-       break;
       }
+       break;
+    }
   }
-
-
-
-
   server.send ( 200, "text/plain", message );
 }
 
@@ -245,7 +208,7 @@ void handleColor(){
   String message = "Color not  found";
   for ( uint8_t i = 0; i < server.args(); i++ ) {
     if( server.argName ( i )  == "color"){
-       hexcolor =  server.arg ( i ) ;
+      hexcolor =  server.arg ( i ) ;
       String past_rgb_val = rgb_value;
       rgb_value = hex2rgb(hexcolor);
       String xval = getValue(rgb_value, ':', 0);
@@ -259,36 +222,13 @@ void handleColor(){
        message = "Past rgb: " + past_rgb_val + "\n"+ "Past brightness: " + String(brightness) + "%\n"+ "Reseived " + server.argName ( i ) + ": #" + hexcolor + "; rgb(" + xval +"," +yval+"," +  zval + ")\n";
        setValue(red*4,green*4,blue*4, brightness);
        Serial.println(message);
-
-
-
        break;
       }
   }
-
-
-
-
   server.send ( 200, "text/plain", message );
 }
 
-void drawGraph() {
-  String out = "";
-  char temp[100];
-  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
-  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
-  out += "<g stroke=\"black\">\n";
-  int y = rand() % 130;
-  for (int x = 10; x < 390; x+= 10) {
-    int y2 = rand() % 130;
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
-    out += temp;
-    y = y2;
-  }
-  out += "</g>\n</svg>\n";
 
-  server.send ( 200, "image/svg+xml", out);
-}
 
 
 
@@ -327,8 +267,6 @@ void openPicker() {
      <script src='https://kantorv.github.io/nodemcu_rgb_lamp/frontend/js/jquery.ui.touch-punch.min.js'></script> \
     </body>\
 </html>",
-
-
 
     hr, min % 60, sec % 60
   );
